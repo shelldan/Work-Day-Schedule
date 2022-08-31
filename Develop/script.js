38dd535101ec92dbacef56f73bdc556f75e533e5
shelldan <shelldanlee@gmail.com>
2022-08-31 14:13:47 -0500

    update

--- Develop/script.js
+++ Develop/script.js
@@ -1,46 +1,67 @@
 var today = new Date();
 var colorCode = document.querySelector(".colorCode"); //DOM selector method 
-var taskInput= document.getElementById('task');
-var submitBtn = document.getElementById('submitBtn');
-var hour = ['9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm']; // hour variable 
+var taskInput= document.querySelector('.task');
+var submitBtn = document.querySelector('.submitBtn');
+var hour = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']; // hour variable 
+var currentHour = (new Date()).getHours();
+
+//console.log(taskInput)
 
 // Date and Time 
 $("#currentDay").text(moment(today).format('MMM Do YYYY, h:mm:ss a'))
 
 //when click submit button, save the task to the local storage 
-submitBtn.addEventListener('click',function(event){
-    event.preventDefault();
-    var task = document.getElementById('task').value;
-    localStorage.setItem('task',JSON.stringify(task)) //setItem(keyName, keyValue), keyName should be the timeblock, and keyValue should be the task 
-    renderLastSavedValue();        
-})
+// submitBtn.addEventListener('click',function(event){
+//     event.preventDefault();
+//     var task = document.getElementById('task').value;
+//     localStorage.setItem('task',JSON.stringify(task)) //setItem(keyName, keyValue), keyName should be the timeblock, and keyValue should be the task 
+//     renderLastSavedValue();        
+// })
 
-function renderLastSavedValue(){
-    var savedTask = JSON.parse(localStorage.getItem('task'));
-    taskInput.textContent = savedTask;
-}
+// function renderLastSavedValue(){
+//     var savedTask = JSON.parse(localStorage.getItem('task'));
+//     taskInput.textContent = savedTask;
+// }
 
-renderLastSavedValue(); //why do we need this function here? why do we need to call the function ?
+// renderLastSavedValue(); //why do we need this function here? why do we need to call the function ?
 
-// can't get the user input after refresh the page
+$(function() {
+    $('.submitBtn').each(function() {
+      $(this).click(function() {
+        //var keyValue = ($(this).parent().prev().prev().children().html() + ' to do');//why localStorage.setItem doesn't take this value? 
+        var keyName = ($(this).parent().prev().children().val()); 
 
+        localStorage.setItem(($(this).parent().prev().prev().children().html() + ' to do'),keyName)
+        renderLastSavedValue()
+      }); 
+    });
+  });
+
+  $(function renderLastSavedValue(){
+    $('.task').each(function(){
+        var savedKeyName = ($(this).parent().prev().children().html() + 'to do')
+        ($(this).html()) = localStorage.getItem(savedKeyName)
+    })
+  });
+
+  
 // colorCode 
-// var currentHour = (new Date()).getHours();
-// $('.colorCode')
-//   .each(function colorCode(){
-//     var val = parseInt($(this).prop('id'));
-//     if(val > currentHour && val < currentHour + 6){
-//       $(this).css('background-color','Blue'); // future
-//     }else if(val < currentHour && val > currentHour-6){
-//       $(this).css('background-color','Red'); // past
-//     }else if(val === currentHour){
-//       $(this).css('background-color','Green'); // present 
-//     }else{
-//       $(this).css('background-color','White');
-//     }
-//   });
 
-var currentHour = (new Date()).getHours();
+$('.hour').each(function(){
+    //console.log($(this).prop('id')) // this referring to the .hour class in the label element 
+    var hourValue = parseInt($(this).prop('id'));
+    //var color = $(this).parent().next().children().html();
+    if (currentHour > hourValue){
+        $(this).parent().next().children().addClass('past') //
+    } else if (currentHour < hourValue) {
+        $(this).parent().next().children().addClass('future') //
+    } else if (currentHour = hourValue) {
+        $(this).parent().next().children().addClass('present') //
+    }
+});
+
+// let text = document.querySelector('.hour').parentElement.nextElementSibling.firstElementChild.innerHTML
+// console.log(text)
 
 
 
