ff459bec9010259b260cb34667dc4cd158ab4145
shelldan <shelldanlee@gmail.com>
2022-08-30 20:36:06 -0500

    update

--- Develop/script.js
+++ Develop/script.js
@@ -11,18 +11,18 @@ $("#currentDay").text(moment(today).format('MMM Do YYYY, h:mm:ss a'))
 submitBtn.addEventListener('click',function(event){
     event.preventDefault();
     var task = document.getElementById('task').value;
-    localStorage.setItem('task',task) //setItem(keyName, keyValue), keyName should be the timeblock, and keyValue should be the task 
+    localStorage.setItem('task',JSON.stringify(task)) //setItem(keyName, keyValue), keyName should be the timeblock, and keyValue should be the task 
     renderLastSavedValue();        
 })
 
 function renderLastSavedValue(){
-    var savedTask = localStorage.getItem('task');
+    var savedTask = JSON.parse(localStorage.getItem('task'));
     taskInput.textContent = savedTask;
 }
 
 renderLastSavedValue(); //why do we need this function here? why do we need to call the function ?
 
-// can't get the user input after refresh the page 
+// can't get the user input after refresh the page
 
 // colorCode 
 var currentHour = (new Date()).getHours();
@@ -30,17 +30,18 @@ $('.colorCode')
   .each(function(){
     var val = parseInt($(this).prop('id'));
     if(val > currentHour && val < currentHour + 6){
-      $(this).css('background-color','Blue');
+      $(this).css('background-color','Blue'); // future
     }else if(val < currentHour && val > currentHour-6){
-      $(this).css('background-color','Red');
+      $(this).css('background-color','Red'); // past
     }else if(val === currentHour){
-      $(this).css('background-color','Green');
+      $(this).css('background-color','Green'); // present 
     }else{
       $(this).css('background-color','White');
     }
   });
 
 
+  
 
 // client-side means the action takes place on the user's (the client's) computer
 // server-side means that the action takes place on a web server 
