ff2e944489e84eb96e1b4645cc64d7a975240029
shelldan <shelldanlee@gmail.com>
2022-08-31 14:48:06 -0500

    update

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 var today = new Date();
 var colorCode = document.querySelector(".colorCode"); //DOM selector method 
-var taskInput= document.querySelector('.task');
+//var taskInput= document.querySelector('.task');
 var submitBtn = document.querySelector('.submitBtn');
 var hour = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']; // hour variable 
 var currentHour = (new Date()).getHours();
@@ -25,29 +25,23 @@ $("#currentDay").text(moment(today).format('MMM Do YYYY, h:mm:ss a'))
 
 // renderLastSavedValue(); //why do we need this function here? why do we need to call the function ?
 
-$(function() {
-    $('.submitBtn').each(function() {
-      $(this).click(function() {
-        //var keyValue = ($(this).parent().prev().prev().children().html() + ' to do');//why localStorage.setItem doesn't take this value? 
-        var keyName = ($(this).parent().prev().children().val()); 
 
-        localStorage.setItem(($(this).parent().prev().prev().children().html() + ' to do'),keyName)
-        renderLastSavedValue()
-      }); 
+$(document).ready(function(){
+
+  $('.submitBtn').each(function(){
+    $(this).click(function(){
+      var keyValue = ($(this).parent().prev().prev().children().html() + ' to do');
+      var keyName = ($(this).parent().prev().children().val());
+      localStorage.setItem(keyValue,keyName);
+
+      var savedKeyName = localStorage(keyValue)
+      console.log(savedKeyName)
+      
     });
   });
 
-  $(function renderLastSavedValue(){
-    $('.task').each(function(){
-        var savedKeyName = ($(this).parent().prev().children().html() + 'to do')
-        ($(this).html()) = localStorage.getItem(savedKeyName)
-    })
-  });
-
-  
-// colorCode 
-
-$('.hour').each(function(){
+  // colorCode - this function controls the colorCode of each time-block 
+  $('.hour').each(function(){
     //console.log($(this).prop('id')) // this referring to the .hour class in the label element 
     var hourValue = parseInt($(this).prop('id'));
     //var color = $(this).parent().next().children().html();
@@ -58,13 +52,17 @@ $('.hour').each(function(){
     } else if (currentHour = hourValue) {
         $(this).parent().next().children().addClass('present') //
     }
-});
+  });
+
+  
+
+})
+  
+
+
 
 // let text = document.querySelector('.hour').parentElement.nextElementSibling.firstElementChild.innerHTML
 // console.log(text)
-
-
-
 
 // client-side means the action takes place on the user's (the client's) computer
 // server-side means that the action takes place on a web server 
